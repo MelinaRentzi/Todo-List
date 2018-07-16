5f100a10c31f936dbe24e07fa5cbe7c360a3f5a1
MelinaRentzi <r.melina_92@hotmail.com>
2018-07-16 16:02:14 +0300

    Add new todos from the text input that can be deleted and marked

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,16 +1,28 @@
 // Check off specific todos by clicking
-$("li").click(function () {
+$("ul").on("click", "li", function () {
     // if li is gray turn it to black, else turn it to gray
     $(this).toggleClass("completed");
 
 });
 
 //Click on X to Delete Todo
-$("span").click(function (event) {
+$("ul").on("click", "span", function (event) {
     $(this).parent().fadeOut(500, function () {
         $(this).remove();
     });
 
     // removes  bubbling
     event.stopPropagation();
+});
+
+$("input[type = 'text']").keypress(function (event) {
+    // the enter key code is 13
+    if (event.which === 13) {
+        //grab text from input
+        var todoText = $(this).val();
+        $(this).val("");
+        //create a new li and add to ul (append)
+        $("ul").append("<li><span>X </span>" + todoText + "</li>");
+
+    }
 });
